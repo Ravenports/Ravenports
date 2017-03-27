5ecda1325da7d34802430d9817eefadf4de1aa9b
John Marino <draco@marino.st>
2017-03-27 18:48:35 -0500

    Add ravensys-uname and patch ports
    This brings in charsetfix module.

---
+++ Mk/Uses/charsetfix.mk
@@ -0,0 +1,19 @@
+# Lookup in Makefile.in to prevent a package from installing/modifying charset.alias
+#
+# Feature:	charsetfix
+# Usage:	USES=charsetfix
+# Valid ARGS:	does not require args
+
+.if !defined(_INCLUDE_USES_CHARSETFIX_MK)
+_INCLUDE_USES_CHARSETFIX_MK=	yes
+
+CHARSETFIX_MAKEFILEIN?=	Makefile.in
+
+_USES_patch+=	600:charsetfix-post-patch
+charsetfix-post-patch:
+	@${FIND} ${WRKSRC} -name "${CHARSETFIX_MAKEFILEIN}" -type f | ${XARGS} ${REINPLACE_CMD} \
+		-e 's|need_charset_alias=true|need_charset_alias=false|g ; \
+		s|test -f $$(charset_alias)|false|g ;\
+		s|test -f $$(DESTDIR)$$(libdir)/charset.alias|false|g'
+
+.endif
