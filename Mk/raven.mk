5f2bb27eca8dc00025941a18a258bee2a5aebc1e
John Marino <draco@marino.st>
2017-05-24 20:03:30 -0500

    Ravenports generated: 24 May 2017 20:03

--- Mk/raven.mk
+++ Mk/raven.mk
@@ -478,7 +478,7 @@ do-clean:
 	@if [ -d ${WRKDIR} ]; then \
 		if [ -w ${WRKDIR} ]; then \
 			${RM} -r ${WRKDIR}; \
-			${FIND} ${WRKDIR:H} -type d -depth -maxdepth 1 -empty -delete; \
+			${FIND} ${WRKDIR:H} -depth -maxdepth 1 -type d -empty -delete; \
 		else \
 			${ECHO_MSG} "===>   ${WRKDIR} not writable, skipping"; \
 		fi; \
