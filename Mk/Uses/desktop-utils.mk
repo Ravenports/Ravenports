41d923b80941c49011ad44a5e05b5ced2730f03a
John Marino <draco@marino.st>
2017-09-09 16:03:59 -0500

    Ravenports generated: 09 Sep 2017 16:03

--- Mk/Uses/desktop-utils.mk
+++ Mk/Uses/desktop-utils.mk
@@ -14,7 +14,7 @@ _INCLUDE_USES_DESKTOP_UTILS_MK=	yes
 _USES_stage+=	934:add-plist-desktop
 
 .  if !target(add-plist-desktop)
-add-plist-desktop
+add-plist-desktop:
 	@if [ -f "${WRKDIR}/.manifest.${desktop-utils_ARGS}.mktmp" ]; then \
 	  echo "@desktop-file-utils" >> ${WRKDIR}/.manifest.${desktop-utils_ARGS}.mktmp; \
 	fi
