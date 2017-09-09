d011e338993d74a6367f81c3095a8b1eb61ef89a
John Marino <draco@marino.st>
2017-09-08 22:17:05 -0500

    Ravenports generated: 08 Sep 2017 22:16

---
+++ Mk/Uses/desktop-utils.mk
@@ -0,0 +1,23 @@
+# Handles dependency on desktop-file-utils and updates subpackage
+# packaging list
+#
+# Feature:	desktop-utils
+# Usage:	USES=desktop-utils
+# Valid ARGS:	subpackage
+#
+# ravenadm add desktop-file-utils:single:standard to RUN_DEPENDS
+#
+
+.if !defined(_INCLUDE_USES_DESKTOP_UTILS_MK)
+_INCLUDE_USES_DESKTOP_UTILS_MK=	yes
+
+_USES_stage+=	934:add-plist-desktop
+
+.  if !target(add-plist-desktop)
+add-plist-desktop
+	@if [ -f "${WRKDIR}/.manifest.${desktop-utils_ARGS}.mktmp" ]; then \
+	  echo "@desktop-file-utils" >> ${WRKDIR}/.manifest.${desktop-utils_ARGS}.mktmp; \
+	fi
+.  endif
+
+.endif	# _INCLUDE_USES_DESKTOP_UTILS_MK
