81d7989f86f37999434bc54d330186d4c27c4c37
John Marino <draco@marino.st>
2018-01-08 21:08:46 -0600

    Ravenports generated: 08 Jan 2018 21:07

--- Mk/raven.information.mk
+++ Mk/raven.information.mk
@@ -149,6 +149,7 @@ LIBFM_VERSION=			1.2.5
 FREI0R_VERSION=			1.6.1
 M17N_VERSION=			1.7.0
 GLIBC_VERSION=			2.26
+POPPLER_VERSION=		0.62.0
 
 # ------------------------------------------------------------------------
 # Gnatcross options
