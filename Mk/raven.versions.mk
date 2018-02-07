9f76788f865d53fde6736c21a9961c8a15f7b16b
John Marino <draco@marino.st>
2018-02-07 09:50:16 -0600

    Ravenports generated: 07 Feb 2018 09:49

--- Mk/raven.versions.mk
+++ Mk/raven.versions.mk
@@ -120,7 +120,7 @@ MYSQL_galera-5.5_VERSION=	5.5.54
 FIREBIRD_DEFAULT?=	2.5
 
 FIREBIRD_2.5_VERSION=	2.5.7
-FIREBIRD_3.0_VERSION=	3.0.2
+FIREBIRD_3.0_VERSION=	3.0.3
 
 #-------------------------------------------------------------------------
 # PHP (restricted to branches still receiving updates)
