069a3d18d030c9d3d8d833fef80684b42461e5f5
John Marino <draco@marino.st>
2018-04-17 09:15:06 -0500

    Ravenports generated: 17 Apr 2018 09:14

--- Mk/raven.information.mk
+++ Mk/raven.information.mk
@@ -104,7 +104,7 @@ LIBXSLT_PYTHON_REVISION=	0
 # ruby
 # ------------------------------------------------------------------------
 
-RUBY_2.3_REVISION=		1
+RUBY_2.3_REVISION=		0
 RUBY_2.3_PATCHLEVEL=		0
 
 RUBY_2.4_REVISION=		0
