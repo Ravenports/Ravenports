a258f59567cd6945905ffd7d2e524b59fa1ec834
John Marino <draco@marino.st>
2018-05-23 11:26:32 -0500

    Ravenports generated: 23 May 2018 11:25

--- Mk/raven.information.mk
+++ Mk/raven.information.mk
@@ -195,6 +195,6 @@ GNATCROSS_FREEBSD_12=		12.0
 # Qt and Lumina versions
 # ------------------------------------------------------------------------
 
-QT5_VERSION=			5.10.1
+QT5_VERSION=			5.11.0
 LUMINA_VERSION=			1.4.1
 LUMINA_RELEASE_TAG=		b6536c1
