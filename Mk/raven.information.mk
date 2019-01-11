8026113c0f2465a8b3441acd3b54bc133adece00
John Marino <draco@marino.st>
2019-01-11 10:44:48 -0600

    Ravenports generated: 11 Jan 2019 10:43

--- Mk/raven.information.mk
+++ Mk/raven.information.mk
@@ -172,7 +172,7 @@ ASPELL_CORE_VERSION=		0.60.6.1
 
 APR1_VERSION=			1.6.5
 APR1_UTIL_VERSION=		1.6.1
-SUBVERSION_VERSION=		1.11.0
+SUBVERSION_VERSION=		1.11.1
 
 # ------------------------------------------------------------------------
 # Miscellaneous master versions
