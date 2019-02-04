ac17f7c52a64a937f9c5d8416b228c69d9417e00
John Marino <draco@marino.st>
2019-02-04 09:42:56 -0600

    Ravenports generated: 04 Feb 2019 09:41

--- Mk/Uses/gem.mk
+++ Mk/Uses/gem.mk
@@ -97,8 +97,13 @@ do-build:
 _USES_install+= 780:post-install-gem
 
 do-install:
-	(cd ${BUILD_WRKSRC} && ${SETENV} ${GEM_ENV} \
-		${RUBYGEMBIN} install ${RUBYGEM_ARGS} ${GEMFILE} -- --build-args ${CONFIGURE_ARGS})
+	if [ -z "${CONFIGURE_ARGS}"]; then \
+	  (cd ${BUILD_WRKSRC} && ${SETENV} ${GEM_ENV} \
+	  ${RUBYGEMBIN} install ${RUBYGEM_ARGS} ${GEMFILE} --) ;\
+	else \
+	  (cd ${BUILD_WRKSRC} && ${SETENV} ${GEM_ENV} \
+	  ${RUBYGEMBIN} install ${RUBYGEM_ARGS} ${GEMFILE} -- --build-args ${CONFIGURE_ARGS}) ;\
+	fi
 	${RM} -r ${STAGEDIR}${PREFIX}/${GEMS_BASE_DIR}/build_info/
 	${FIND} ${STAGEDIR}${PREFIX}/${GEMS_BASE_DIR} -type f -name '*.so' -exec ${STRIP_CMD} {} +
 	${FIND} ${STAGEDIR}${PREFIX}/${GEMS_BASE_DIR} -type f \( -name mkmf.log -or -name gem_make.out \) -delete
