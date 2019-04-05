cb26627d5449208c0063c64db4daf58b50b82841
John Marino <draco@marino.st>
2019-04-05 14:01:04 -0500

    Ravenports generated: 05 Apr 2019 14:00

--- Mk/raven.information.mk
+++ Mk/raven.information.mk
@@ -170,7 +170,7 @@ ASPELL_CORE_VERSION=		0.60.6.1
 # apr1 and other apache projects
 # ------------------------------------------------------------------------
 
-APR1_VERSION=			1.6.5
+APR1_VERSION=			1.7.0
 APR1_UTIL_VERSION=		1.6.1
 SUBVERSION_VERSION=		1.11.1
 
