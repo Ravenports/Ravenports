e1343d4f791640a28225c616c8918abaf020352d
John Marino <draco@marino.st>
2021-01-21 15:40:33 +0000

    Ravenports generated: 21 Jan 2021 15:40

--- Mk/raven.information.mk
+++ Mk/raven.information.mk
@@ -154,7 +154,7 @@ GTK3_VERSION=			3.0.0
 GTK4_VERSION=			4.0.0
 GTK2_PORT_VERSION=		2.24.33
 GTK3_PORT_VERSION=		3.24.24
-GTK4_PORT_VERSION=		4.0.1
+GTK4_PORT_VERSION=		4.0.2
 
 # ------------------------------------------------------------------------
 # default gcc and binutils
