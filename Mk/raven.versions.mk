e8a9c5a44ee558fae8e7ff77de09e3d4f67672b0
John Marino <draco@marino.st>
2021-05-03 16:45:54 +0000

    Ravenports generated: 03 May 2021 16:45

--- Mk/raven.versions.mk
+++ Mk/raven.versions.mk
@@ -129,9 +129,9 @@ FIREBIRD_3.0_VERSION=	3.0.7
 
 PHP_DEFAULT?=		7.4
 
-PHP_7.3_VERSION=	7.3.27
-PHP_7.4_VERSION=	7.4.16
-PHP_8.0_VERSION=	8.0.3
+PHP_7.3_VERSION=	7.3.28
+PHP_7.4_VERSION=	7.4.18
+PHP_8.0_VERSION=	8.0.5
 
 #-------------------------------------------------------------------------
 # Ravenports system root versions per platform
