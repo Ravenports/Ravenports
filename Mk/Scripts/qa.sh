38fbc823704cb7ef64c6efabf162e583325b0dd6
John Marino <draco@marino.st>
2021-07-04 17:59:14 +0000

    Ravenports generated: 04 Jul 2021 17:59

--- Mk/Scripts/qa.sh
+++ Mk/Scripts/qa.sh
@@ -49,10 +49,10 @@ shebangonefile() {
 		;;
 	${LOCALBASE}/bin/perl5.* | ${PREFIX}/bin/perl5.*)
 		# perl ports are allowed to have these shebangs.
-		if [ "${NAMEBASE}" != "perl-5.28" \
+		if [ "${NAMEBASE}" != "perl-5.34" \
 		  -a "${NAMEBASE}" != "perl-5.30" \
 		  -a "${NAMEBASE}" != "perl-5.32" \
-		  -a "${VARIANT}" != "528" \
+		  -a "${VARIANT}" != "534" \
 		  -a "${VARIANT}" != "530" \
 		  -a "${VARIANT}" != "532" ]; then
 			err "'${interp}' is an invalid shebang for '${f#${STAGEDIR}${PREFIX}/}' you must use ${LOCALBASE}/bin/perl."
