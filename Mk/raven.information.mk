222c57e056454f9823c1c92be65f1735e52647a2
John Marino <draco@marino.st>
2022-02-07 00:54:17 +0000

    Ravenports generated: 07 Feb 2022 00:54

--- Mk/raven.information.mk
+++ Mk/raven.information.mk
@@ -223,6 +223,6 @@ GNATCROSS_NETBSD_9=		9.2
 # ------------------------------------------------------------------------
 
 QT5_VERSION=			5.15.2
-QT6_VERSION=			6.2.2
+QT6_VERSION=			6.2.3
 LUMINA_VERSION=			1.6.2
 LUMINA_RELEASE_TAG=		1.6.2
