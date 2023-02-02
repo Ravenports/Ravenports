29b2204e60c9247206f2e3064c29353932e4dfab
John Marino <draco@marino.st>
2023-02-02 02:37:47 +0000

    Ravenports generated: 02 Feb 2023 02:37

--- Mk/Uses/jpeg.mk
+++ Mk/Uses/jpeg.mk
@@ -2,7 +2,7 @@
 #
 # Feature:	jpeg
 # Usage:	USES=jpeg or USES=jpeg:ARGS
-# Valid ARGS:	buildrun (default), build, run
+# Valid ARGS:	buildrun (implicit), build
 #
 
 .if !defined(_INCLUDE_USES_JPEG_MK)
@@ -11,14 +11,12 @@ _INCLUDE_USES_JPEG_MK=	yes
 # -----------------------------------------------------
 # Incorporated in ravenadm
 # -----------------------------------------------------
-# if ARGS contain :build (but not :run, :buildrun)
-# BUILD_DEPENDS+=	jpeg-turbo:primary:standard
-# -----------------------------------------------------
-# if ARGS contain nothing or :buildrun (or :build and :run)
-# BUILDRUN_DEPENDS+=   jpeg-turbo:primary:standard
-# -----------------------------------------------------
-# if ARGS contains :run (and not :build, :buildrun)
-# RUN_DEPENDS+=        jpeg-turbo:primary:standard
+#.if {jpeg_ARGS:Mbuild}
+# BUILD_DEPENDS+=	jpeg-turbo:dev:standard
+#.else
+# BUILD_DEPENDS+=	jpeg-turbo:dev:standard
+# BUILDRUN_DEPENDS+=	jpeg-turbo:primary:standard
+#.endif
 # -----------------------------------------------------
 
 .endif
