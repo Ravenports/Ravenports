c69be2c9239753cb7c55e73c5408fd366b2e2c68
John Marino <draco@marino.st>
2023-03-20 01:11:51 +0000

    Ravenports generated: 20 Mar 2023 01:11

--- Mk/raven.information.mk
+++ Mk/raven.information.mk
@@ -191,7 +191,7 @@ GTK3_VERSION=			3.0.0
 GTK4_VERSION=			4.0.0
 GTK2_PORT_VERSION=		2.24.33
 GTK3_PORT_VERSION=		3.24.37
-GTK4_PORT_VERSION=		4.10.0
+GTK4_PORT_VERSION=		4.10.1
 
 # ------------------------------------------------------------------------
 # Ravenports binutils
--- Mk/raven.versions.mk
+++ Mk/raven.versions.mk
@@ -145,8 +145,8 @@ FIREBIRD_4.0_VERSION=	4.0.2
 PHP_DEFAULT?=		8.1
 
 PHP_8.0_VERSION=	8.0.28
-PHP_8.1_VERSION=	8.1.16
-PHP_8.2_VERSION=	8.2.3
+PHP_8.1_VERSION=	8.1.17
+PHP_8.2_VERSION=	8.2.4
 
 #-------------------------------------------------------------------------
 # Ravenports system root versions per platform
