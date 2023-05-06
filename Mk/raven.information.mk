c3003aacc1f682b917522565b7046215a715c6df
John Marino <draco@marino.st>
2023-05-06 21:29:19 +0000

    Ravenports generated: 06 May 2023 21:29

--- Mk/raven.information.mk
+++ Mk/raven.information.mk
@@ -129,7 +129,7 @@ ICU_LX_REVISION=		0
 # libxml2
 # ------------------------------------------------------------------------
 
-LIBXML2_VERSION=		2.11.1
+LIBXML2_VERSION=		2.11.2
 LIBXML2_REVISION=		0
 LIBXML2_PYTHON_REVISION=	0
 
