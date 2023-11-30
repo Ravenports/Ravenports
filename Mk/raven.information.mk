aa21c8f3555bef35449935755565857f3945dcda
John Marino <draco@marino.st>
2023-11-30 17:31:18 +0000

    Ravenports generated: 30 Nov 2023 17:31

--- Mk/raven.information.mk
+++ Mk/raven.information.mk
@@ -121,7 +121,7 @@ GCCBASE_PHASE=			release
 # icu
 # ------------------------------------------------------------------------
 
-ICU_VERSION=			73.2
+ICU_VERSION=			74.1
 ICU_REVISION=			1
 ICU_LX_REVISION=		0
 
@@ -129,7 +129,7 @@ ICU_LX_REVISION=		0
 # libxml2
 # ------------------------------------------------------------------------
 
-LIBXML2_VERSION=		2.12.0
+LIBXML2_VERSION=		2.12.1
 LIBXML2_REVISION=		0
 LIBXML2_PYTHON_REVISION=	0
 
