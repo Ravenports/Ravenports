63a5bd82f531a7104d2eb5039cd9b93b90a2d719
John Marino <draco@marino.st>
2025-01-29 04:36:39 +0000

    Ravenports generated: 29 Jan 2025 04:36

--- Mk/Uses/gettext.mk
+++ Mk/Uses/gettext.mk
@@ -11,22 +11,27 @@ _INCLUDE_USES_GETTEXT_MK=	yes
 # -----------------------------------------------------
 # Incorporated in ravenadm
 # -----------------------------------------------------
-# BUILD_DEPENDS+=	gettext:dev:std
+# BUILD_DEPENDS+=	gettext:dev:std        (except glibc)
 # BUILD_DEPENDS+=	gettext:bldtools:std
 # BUILD_DEPENDS+=	gettext:tools:std
 #
 #.if ! ${gettext_ARGS:Mbuild}
 # BUILD_DEPENDS+=	gettext:solinks:std
-# BUILDRUN_DEPENDS+=	gettext:primary:std
-#.  if ${gettext_ARGS:Masprintf}
+# BUILDRUN_DEPENDS+=	gettext:primary:std    (except glibc)
+#.endif
+#
+#.if ${gettext_ARGS:Masprintf}
+# BUILD_DEPENDS+=       gettext:asprintfdev:std
 # BUILDRUN_DEPENDS+=    gettext:asprintf:std
-#.  endif
 #. endif
 # -----------------------------------------------------
 
 # for cargo support (only in effect with USES=cargo)
-CARGO_ENV+=	GETTEXT_BIN_DIR=${LOCALBASE}/bin \
-		GETTEXT_INCLUDE_DIR=${LOCALBASE}/include \
+CARGO_ENV+=	GETTEXT_BIN_DIR=${LOCALBASE}/bin
+
+.  if !exists(/usr/include/libintl.h)
+CARGO_ENV+=	GETTEXT_INCLUDE_DIR=${LOCALBASE}/include \
 		GETTEXT_LIB_DIR=${LOCALBASE}/lib
+.  endif
 
 .endif
--- Mk/Uses/iconv.mk
+++ Mk/Uses/iconv.mk
@@ -1,12 +1,17 @@
-# handle dependency on the iconv port
+# handle dependency on the libiconv port
 #
 # Feature:	iconv
 # Usage:	USES=iconv or USES=iconv:ARGS
-# Valid ARGS:	buildrun (default, implicit), build
+# Valid ARGS:	buildrun (default, implicit), build, standalone
 #
+# Libiconv is not added to systems with iconv supported by glibc
+# unless "standalone" argument is set.
 
 .if !defined(_INCLUDE_USES_ICONV_MK)
 _INCLUDE_USES_ICONV_MK=	yes
+
+
+.  if !exists(/usr/include/libintl.h) || ${iconv_ARGS:Mstandalone}
 
 ICONV_CMD=		${LOCALBASE}/bin/iconv
 ICONV_LIB=		-liconv
@@ -16,17 +21,6 @@ ICONV_CONFIGURE_BASE=	--with-libiconv=${LOCALBASE}
 ICONV_INCLUDE_PATH=	${LOCALBASE}/include
 ICONV_LIBRARY_PATH=	${LOCALBASE}/lib
 ICONV_LIBRARY=		${LOCALBASE}/lib/libiconv.so
-
-# -----------------------------------------------
-# Incorporated in ravenadm
-# -----------------------------------------------
-#.if {iconv_ARGS:Mbuild}
-# BUILD_DEPENDS+=	libiconv:dev:std
-#.else
-# BUILD_DEPENDS+=	libiconv:dev:std
-# BUILDRUN_DEPENDS+=	libiconv:primary:std
-#.endif
-# -----------------------------------------------
 
 # These are the most common names for the iconv-related variables found in
 # CMake-based ports. We set them here via CMAKE_ARGS to make sure that the
@@ -40,5 +34,30 @@ CMAKE_ARGS+=	-DICONV_INCLUDE_DIR=${ICONV_INCLUDE_PATH} \
 		-DLIBICONV_INCLUDE_DIR=${ICONV_INCLUDE_PATH} \
 		-DLIBICONV_LIBRARIES=${ICONV_LIBRARY} \
 		-DLIBICONV_LIBRARY=${ICONV_LIBRARY}
+
+# -----------------------------------------------
+# Incorporated in ravenadm
+# -----------------------------------------------
+#.if ${iconv_ARGS:Mbuild}
+# BUILD_DEPENDS+=	libiconv:dev:std
+#.else
+# BUILD_DEPENDS+=	libiconv:dev:std
+# BUILDRUN_DEPENDS+=	libiconv:primary:std
+#.endif
+# -----------------------------------------------
+
+
+.  else
+
+ICONV_CMD=		/usr/bin/iconv
+ICONV_LIB=
+ICONV_PREFIX=		/usr
+ICONV_CONFIGURE_ARG=
+ICONV_CONFIGURE_BASE=
+ICONV_INCLUDE_PATH=	/usr/include
+ICONV_LIBRARY_PATH=	/usr/lib
+ICONV_LIBRARY=
+
+.  endif
 
 .endif
