8ff795525904203584eb9a3c3065438d338c25b2
John Marino <draco@marino.st>
2025-02-21 19:12:19 +0000

    Ravenports generated: 21 Feb 2025 19:12

--- Mk/raven.information.mk
+++ Mk/raven.information.mk
@@ -111,7 +111,7 @@ ICU_LX_REVISION=		0
 # libxml2
 # ------------------------------------------------------------------------
 
-LIBXML2_VERSION=		2.13.5
+LIBXML2_VERSION=		2.13.6
 LIBXML2_REVISION=		0
 LIBXML2_PYTHON_REVISION=	0
 
