35545971c1f7d0f1d2e1a2e60b9f357bd7db3742
John Marino <draco@marino.st>
2025-07-12 16:58:48 +0000

    Ravenports generated: 12 Jul 2025 16:58

--- Mk/raven.versions.mk
+++ Mk/raven.versions.mk
@@ -127,9 +127,9 @@ MYSQL_mariadb-11.8_VERSION=	11.8.2
 
 PHP_DEFAULT?=		8.3
 
-PHP_8.2_VERSION=	8.2.28
-PHP_8.3_VERSION=	8.3.22
-PHP_8.4_VERSION=	8.4.8
+PHP_8.2_VERSION=	8.2.29
+PHP_8.3_VERSION=	8.3.23
+PHP_8.4_VERSION=	8.4.9
 
 #-------------------------------------------------------------------------
 # Ravenports system root versions per platform
