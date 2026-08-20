574fb10ab278a57797641e1fedfe92d90d2e89e7
John Marino <draco@marino.st>
2026-08-20 11:37:49 +0000

    Ravenports generated: 20 Aug 2026 11:37

--- Mk/raven.versions.mk
+++ Mk/raven.versions.mk
@@ -128,9 +128,9 @@ MYSQL_mariadb-BE_VERSION=	12.3.2		# Bleeding Edge
 
 PHP_DEFAULT?=		8.4
 
-PHP_8.3_VERSION=	8.3.32
-PHP_8.4_VERSION=	8.4.23
-PHP_8.5_VERSION=	8.5.8
+PHP_8.3_VERSION=	8.3.33
+PHP_8.4_VERSION=	8.4.24
+PHP_8.5_VERSION=	8.5.9
 
 #-------------------------------------------------------------------------
 # Ravenports system root versions per platform
